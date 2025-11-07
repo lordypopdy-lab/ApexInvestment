5052afa0f22c9e56d5598206ee884b91d891d5a3
James Isaac <deelordpopdy2@gmail.com>
2025-11-07 03:06:24 -0800

    Update App.jsx
    Safe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import { Toaster } from 'react-hot-toast';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-axios.defaults.baseURL = 'https://apex-investment-server.vercel.appp';
+axios.defaults.baseURL = 'https://apex-investment-server.vercel.app';
 axios.defaults.withCredentials = true;
 
 //http://localhost:8080 
